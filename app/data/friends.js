3a245d727054b9b9da7c8e168e0cb5b1ccdb4b45
kbub27 <kalebmerriman27@gmail.com>
2019-05-21 21:15:45 -0700

    finished routes and debugged some syntax errors with wrong words

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var people = [
     {
         name: 'John Krebser',
-        photo: '',
+        photo: 'https://www.google.com/imgres?imgurl=http%3A%2F%2Fstatic5.businessinsider.com%2Fimage%2F4f86167c69bedd0737000002%2Famit-kapur.jpg&imgrefurl=https%3A%2F%2Fwww.sfgate.com%2Fnews%2Farticle%2FThese-Ex-MySpace-Execs-Want-To-Make-The-Web-More-3529722.php&docid=xNcLYV-JvXwsQM&tbnid=hwYWoBIQB3-FyM%3A&vet=10ahUKEwjcs9eQj67iAhVNip4KHVILC-0QMwjLASgMMAw..i&w=400&h=300&hl=en&authuser=0&bih=818&biw=1707&q=personal%20photo&ved=0ahUKEwjcs9eQj67iAhVNip4KHVILC-0QMwjLASgMMAw&iact=mrc&uact=8',
         scores: [1,2,3,4,5,6,7,8,9,10]
     }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,6 +11,31 @@ module.exports = function (app) {
 
     app.post('/api/friendslist', function (req, res) {
         console.log(req.body);
-        res.send(req.body);
+
+        var userInput = req.body;
+
+        var userScores = userInput.scores;
+
+        var friendName = '';
+        var friendImage = '';
+        var friendDifference = 5000
+        
+
+        for (let i = 0; i < people.length; i++) {
+            var diff = 0;
+            for (let k = 0; k < userScores.length; k++) {
+                diff += Math.abs(people[i].scores[k] - userScores[k]);
+            }
+
+            if (diff < friendDifference) {
+                friendDifference = diff;
+                friendName = people[i].name;
+                friendImage = people[i].photo;
+            };
+        }
+
+        people.push(userInput);
+
+        res.json({ status: 'OK', friendName: friendName, friendImage: friendImage, friendDifference: friendDifference });
     })
 };
