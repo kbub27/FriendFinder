e1115ce1132ca2a0120e19a12cf3963bbd016c56
kbub27 <kalebmerriman27@gmail.com>
2019-05-22 18:37:57 -0700

    added actual people to the friends arr

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,213 +1,33 @@
 var people = [
     {
-        name: 'John Krebser',
+        name: 'Ed Sheeran',
         photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
         scores: [1,2,3,4,5,5,4,3,2,1]
     },
     {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
+        name: 'Naruto',
+        photo: '',
+        scores: [5,2,3,4,5,5,4,3,2,5]
     },
     {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
+        name: 'Boruto',
+        photo: '',
+        scores: [4,3,3,4,5,5,4,3,2,3]
     },
     {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
+        name: 'Ichigo',
+        photo: '',
+        scores: [4,4,3,4,5,5,4,3,2,5]
     },
     {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
+        name: 'Dane Cook',
+        photo: '',
+        scores: [1,5,3,4,5,1,4,3,2,3]
     },
     {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
-    },
-    {
-        name: 'John Krebser',
-        photo: 'https://media.pitchfork.com/photos/592c55445e6ef95969326b55/2:1/w_790/4e9fe179.jpg',
-        scores: [1,2,3,4,5,5,4,3,2,1]
+        name: 'Michael Jordan',
+        photo: '',
+        scores: [5,2,3,4,5,1,4,4,2,1]
     }
 ];
 
