b3a003b4c4c9ca383f58b480ced2a9919ef30d25
kbub27 <kalebmerriman27@gmail.com>
2019-05-22 18:45:24 -0700

    added images for all the new friends in the array

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,27 +6,27 @@ var people = [
     },
     {
         name: 'Naruto',
-        photo: '',
+        photo: 'https://i.kym-cdn.com/entries/icons/mobile/000/015/163/narutoooh.jpg',
         scores: [5,2,3,4,5,5,4,3,2,5]
     },
     {
         name: 'Boruto',
-        photo: '',
+        photo: 'https://vignette.wikia.nocookie.net/naruto/images/b/bd/Boruto_uzumaki.png/revision/latest/scale-to-width-down/300?cb=20150416123755',
         scores: [4,3,3,4,5,5,4,3,2,3]
     },
     {
         name: 'Ichigo',
-        photo: '',
+        photo: 'https://imgix.ranker.com/user_node_img/50081/1001610724/original/the-normal-state-photo-u1?w=650&q=50&fm=pjpg&fit=crop&crop=faces',
         scores: [4,4,3,4,5,5,4,3,2,5]
     },
     {
         name: 'Dane Cook',
-        photo: '',
+        photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Dane_Cook.jpg/330px-Dane_Cook.jpg',
         scores: [1,5,3,4,5,1,4,3,2,3]
     },
     {
         name: 'Michael Jordan',
-        photo: '',
+        photo: 'https://cdn.vox-cdn.com/thumbor/1mn802R0QAUQps-48eGlupEQtqw=/440x552:1811x3072/1220x813/filters:focal(1045x718:1371x1044):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/55248187/245168.0.jpg',
         scores: [5,2,3,4,5,1,4,4,2,1]
     }
 ];
